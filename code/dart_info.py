791908a3d0a4c4f4229b41f57496e987380aa676
Mattnolan45 <matthewnolan.nolan@gmail.com>
2019-01-31 16:48:08 +0000

    Dart Update

--- code/dart_info.py
+++ code/dart_info.py
@@ -2,25 +2,49 @@ import requests
 import xml.etree.ElementTree as ET
 
 
-def get_station(station, direction=''):
+def print_trains(start, index, var, train_type):
     r = requests.get(
-        'http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByNameXML?StationDesc=' + station + '&NumMins=10')
+        'http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByNameXML?StationDesc=' + start + '&NumMins=10')
     tree = ET.fromstring(r.text)
+    info = [[tree[i][j].text for j in range(len(tree[i]))] for i in range(len(tree))]
+    count = 0
+    for i in range(len(info)):
+        if info[i][10] == 'En Route' and info[i][index] == var and info[i][19] == train_type:
+            print("Destination: {} ETA: {} Status: {}".format(info[i][7], info[i][14], info[i][11]))
+            count += 1
+    if count == 0:
+        print("No Trains Running")
 
-    l = [[tree[i][j].text for j in range(len(tree[i]))] for i in range(len(tree))]
 
-    for i in range(len(l)):
-        if l[i][10] == 'En Route':
-            print("Destination: {} ETA: {} Status: {}".format(l[i][7], l[i][14], l[i][11]))
+def find_dart_destination(start, finish):
+    stations = ["Greystones", "Bray", "Shankill", "Killiney", "Dalkey", "Glenageary", "Sandycove and Glasthule",
+                "Dun Laoghaire", "Salthill and Monkstown", "Seapoint", "Booterstown", "Sydney Parade", "Sandymount",
+                "Lansdowne Road", "Grand Canal Dock", "Pearse", "Tara Street", "Connolly", "Clontarf Road", "Killester",
+                "Harmonstown", "Raheny", "Kilbarrack", "Howth Junction"]
+    malahide = ["Clongriffen", "Portmarnock", "Malahide"]
+    howth = ["Bayside", "Sutton", "Howth"]
 
-
-
+    if finish in malahide:
+        print_trains(start, 7, "Malahide", "DART")
+    elif finish in howth:
+        print_trains(start, 7, "Howth", "DART")
+    else:
+        if start in malahide or start and finish in stations:
+            print_trains(start, 18, "Southbound", "DART")
+        elif stations.index(start) > stations.index(finish):
+            print_trains(start, 18, "Southbound", "DART")
+        elif stations.index(start) < stations.index(finish):
+            print_trains(start, 18, "Northbound", "DART")
+        elif start not in stations or finish not in stations:
+            print("Unable to find station")
 
 
 def all_stations():
     r = requests.get("http://api.irishrail.ie/realtime/realtime.asmx/getAllStationsXML")
     tree = ET.fromstring(r.text)
-    print(tree)
+    station_list = [[tree[i][j].text for j in range(len(tree[i]))] for i in range(len(tree))]
+    for n in range(len(station_list)):
+        print(station_list[n])
 
 
 def chat():
@@ -28,14 +52,25 @@ def chat():
     if (input('Hello there!!\nAre you travelling by train today?')) in yes:
         if (input("Are you looking for information for a particular station?")) == 'yes':
             stat = input("Which station?")
-            dest = input("What direction are you going Northbound or Southbound?")
-            get_station(stat, dest)
-
+            dest = input("What is your destination?")
+            find_dart_destination(stat, dest)
     else:
         print('Unable to help')
     input()
 
 
 if __name__ == '__main__':
-    #chat()
-    get_station('Raheny',)
+    # all_stations()
+    print_trains("Howth", 18, "Northbound", "Train")
+    """
+    print("\nMALAHIDE\n")
+    find_dart_destination("Clontarf Road", "Portmarnock")
+    print("\nHOWTH\n")
+    find_dart_destination("Malahide", "Raheny")
+    print("\nNorthbound\n")
+    find_dart_destination("Clontarf Road", "Kilbarrack")
+    print("\nSouthbound\n")
+    find_dart_destination("Portmarnock", "Sandymount")
+
+    chat()
+    """
--- code/testing.py
+++ code/testing.py
@@ -1,59 +1,50 @@
-"""
-import requests
-import xml.etree.ElementTree as ET
-r=requests.get('http://api.irishrail.ie/realtime/realtime.asmx/getAllStationsXML')
-
-tree = ET.fromstring(r.text)
-
-all_stations = [[tree[i][j].text for j in range(len(tree[i]))] for i in range(len(tree))]
-#print(all_stations)
-
-for n in range(len(all_stations)):
-    print(all_stations[n])
-"""
 import requests
 import xml.etree.ElementTree as ET
 
 
-def get_station(station, destination):
+def print_trains(start, index, var, train_type):
     r = requests.get(
-        'http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByNameXML?StationDesc=' + station + '&NumMins=10')
+        'http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByNameXML?StationDesc=' + start + '&NumMins=10')
     tree = ET.fromstring(r.text)
-    l = [[tree[i][j].text for j in range(len(tree[i]))] for i in range(len(tree))]
-    find_dart_destination(l,station,destination)
+    info = [[tree[i][j].text for j in range(len(tree[i]))] for i in range(len(tree))]
+    count = 0
+    for i in range(len(info)):
+        if info[i][10] == 'En Route' and info[i][index] == var and info[i][19] == train_type:
+            print("Destination: {} ETA: {} Status: {}".format(info[i][7], info[i][14], info[i][11]))
+            count += 1
+    if count == 0:
+        print("No Trains Running")
 
 
-def find_dart_destination(info, start, finish):
+def find_dart_destination(start, finish):
     stations = ["Greystones", "Bray", "Shankill", "Killiney", "Dalkey", "Glenageary", "Sandycove and Glasthule",
-                "Dun Laoghaire", "Salthill and Monkstown", "Seapoint", "Booterstown","Sydney Parade", "Sandymount",
-                "Lansdowne Road", "Grand Canal Dock", "Pearse", "Tara Street", "Connolly","Clontarf Road","Killester",
+                "Dun Laoghaire", "Salthill and Monkstown", "Seapoint", "Booterstown", "Sydney Parade", "Sandymount",
+                "Lansdowne Road", "Grand Canal Dock", "Pearse", "Tara Street", "Connolly", "Clontarf Road", "Killester",
                 "Harmonstown", "Raheny", "Kilbarrack", "Howth Junction"]
     malahide = ["Clongriffen", "Portmarnock", "Malahide"]
-    howth = ["Bayside","Sutton", "Howth"]
+    howth = ["Bayside", "Sutton", "Howth"]
 
     if finish in malahide:
-        for i in range(len(info)):
-            if info[i][10] == 'En Route' and info[i][7] == "Malahide":
-                print("Destination: {} ETA: {} Status: {}".format(info[i][7], info[i][14], info[i][11]))
+        print_trains(start, 7, "Malahide", "DART")
     elif finish in howth:
-        for i in range(len(info)):
-            if info[i][10] == 'En Route' and info[i][7] == "Howth":
-                print("Destination: {} ETA: {} Status: {}".format(info[i][7], info[i][14], info[i][11]))
+        print_trains(start, 7, "Howth", "DART")
     else:
-        if stations.index(start) > stations.index(finish):
-            for i in range(len(info)):
-                if info[i][10] == 'En Route' and info[i][18] == "Southbound":
-                    print("Destination: {} ETA: {} Status: {}".format(info[i][7], info[i][14], info[i][11]))
+        if start in malahide or start and finish in stations:
+            print_trains(start, 18, "Southbound", "DART")
+        elif stations.index(start) > stations.index(finish):
+            print_trains(start, 18, "Southbound", "DART")
         elif stations.index(start) < stations.index(finish):
-            for i in range(len(info)):
-                if info[i][10] == 'En Route' and info[i][18] == "Northbound":
-                    print("Destination: {} ETA: {} Status: {}".format(info[i][7], info[i][14], info[i][11]))
+            print_trains(start, 18, "Northbound", "DART")
+        elif start not in stations or finish not in stations:
+            print("Unable to find station")
 
 
 def all_stations():
     r = requests.get("http://api.irishrail.ie/realtime/realtime.asmx/getAllStationsXML")
     tree = ET.fromstring(r.text)
-    print(tree)
+    station_list = [[tree[i][j].text for j in range(len(tree[i]))] for i in range(len(tree))]
+    for n in range(len(station_list)):
+        print(station_list[n])
 
 
 def chat():
@@ -62,22 +53,24 @@ def chat():
         if (input("Are you looking for information for a particular station?")) == 'yes':
             stat = input("Which station?")
             dest = input("What is your destination?")
-            get_station(stat, dest)
-
+            find_dart_destination(stat, dest)
     else:
         print('Unable to help')
     input()
 
 
 if __name__ == '__main__':
+    # all_stations()
+    print_trains("Howth", 18, "Northbound", "Train")
+    """
     print("\nMALAHIDE\n")
-    get_station("Raheny", "Portmarnock")
+    find_dart_destination("Clontarf Road", "Portmarnock")
     print("\nHOWTH\n")
-    get_station("Raheny", "Bayside")
+    find_dart_destination("Malahide", "Raheny")
     print("\nNorthbound\n")
-    get_station("Clontarf Road", "Kilbarrack")
+    find_dart_destination("Clontarf Road", "Kilbarrack")
     print("\nSouthbound\n")
-    get_station("Raheny", "Sandymount")
+    find_dart_destination("Portmarnock", "Sandymount")
 
     chat()
-
+    """
