fa1d841fc2462e68abb91a9362c9f4ff5fa1188d
Mattnolan45 <matthewnolan.nolan@gmail.com>
2019-02-21 14:13:36 +0000

    update

--- code/recieve_whatsApp_sms.py
+++ code/recieve_whatsApp_sms.py
@@ -1,18 +1,38 @@
-from flask import Flask, request, redirect
-from twilio.twiml.messaging_response import MessagingResponse
+from flask import Flask, request, make_response
+from dart_info import *
+import json
+
 
 app = Flask(__name__)
 
 
-@app.route("/sms", methods=["GET", "POST"])
+@app.route("/")
+def disply():
+    return "<h1>Hullo</h1>"
+
+
+@app.route("/sms", methods=["POST"])
 def sms_reply():
-    msg_body = request.form['Body']
 
-    #msg_body = msg_body.split()
-    resp = MessagingResponse()
-    resp.message(reply(msg_body))
+    req = request.get_json(silent=True, force=True)
 
-    return str(resp)
+    result = req.get("queryResult")
+    parameters = result.get("parameters")
+    station1, station2 = parameters.get("train_station"), parameters.get("train_station1")
+    m = print_trains(station1,station2)
+
+    msg = {
+        "fulfillmentText": m,
+        "source": m
+        }
+
+    res = json.dumps(msg , indent=4)
+
+
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
 
 
 if __name__ == "__main__":
